cd6fee8c8591b0e4db9d7b8c8cb4cd0979ee8304
wanglin07070-a11y <wanglin07070@gmail.com>
2026-08-24 04:50:31 +0800

    Update tiktok.js

--- api/tiktok.js
+++ api/tiktok.js
@@ -1,15 +1,6 @@
-export default async function handler(req, res) {
-
-  if (req.method !== "POST") {
-    return res.status(200).json({
-      status:"ok",
-      message:"TikTok API running"
-    });
-  }
-
-
-  return res.status(200).json({
-    success:true
+export default function handler(req, res) {
+  res.status(200).json({
+    success: true,
+    message: "API OK"
   });
-
 }
